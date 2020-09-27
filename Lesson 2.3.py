2a8976ae926163e0fadd2e413ac2ec90285d491d
Ashotm <gmartirosyan@gmail.com>
2020-09-28 01:12:43 +0400

    Lesson 2.1

--- Lesson 2.3.py
+++ Lesson 2.3.py
@@ -5,4 +5,16 @@ print(Matrix1)
 np.median(Matrix1, axis=1)
 print(Matrix1.max())
 print(Matrix1.min())
+
 #Task 2
+print(Matrix1[:,1].max())
+print(Matrix1[:,1].min())
+
+#Task 3
+print(np.median(Matrix1))
+
+
+#Task 4
+Matrix2 = np.random.randint(1,20,size=[2,2])
+Matrix3 = np.random.randint(1,20,size=[2,6])
+print(np.dot(Matrix2, Matrix3))
